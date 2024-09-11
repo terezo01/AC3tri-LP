1a365dcf76902ad0111fd492cf1bb9c610913a7c
terezo01 <thegames0079@gmail.com>
2024-09-11 09:14:48 -0300

    estacionamento adjust

--- estacionamento/estacionamento.js
+++ estacionamento/estacionamento.js
@@ -1,11 +1,11 @@
-function calcular(){
+function calcular() {
     Entrada = parseInt(document.getElementById("hrEntrada").value);
     Saida = parseInt(document.getElementById("hrSaida").value);
 
     diferença = Saida - Entrada
 
-    precoFinal = diferença*15
+    precoFinal = diferença * 15
 
-    
+    document.getElementById('saida').innerHTML = `O preço após ${diferença} horas é de: ${precoFinal}R$`
 
 }
