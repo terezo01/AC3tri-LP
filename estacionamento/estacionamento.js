a873f6ba46f8226a8b6b0e525c5ca9fd7a9287a9
terezo01 <thegames0079@gmail.com>
2024-09-11 10:58:43 -0300

    Estacionamento update

--- estacionamento/estacionamento.js
+++ estacionamento/estacionamento.js
@@ -1,10 +1,41 @@
 function calcular() {
-    Entrada = parseInt(document.getElementById("hrEntrada").value);
-    Saida = parseInt(document.getElementById("hrSaida").value);
+    preço = 0
 
-    diferença = Saida - Entrada
+    Entrada = document.getElementById("Entrada").value;
+    Saida = document.getElementById("Saida").value;
 
-    precoFinal = diferença * 15
+    if(Entrada === '' || Saida === ''){
+        alert('Preencha todos os Campos')
+        return
+    }
+
+    entradaD = new Date(Entrada).getDate();
+    entradaH = new Date(Entrada).getHours();
+    entradaM = new Date(Entrada).getMinutes();
+
+    saidaD = new Date(Saida).getDate();
+    saidaH = new Date(Saida).getHours();
+    saidaM = new Date(Saida).getMinutes();
+
+    if(saidaD === entradaD){
+        diferença = (saidaH - entradaH)
+    }else if(saidaD !== entradaD){
+        diferença = (saidaD - entradaD)*24
+        if(saidaH <= entradaH){
+            diferença -= (entradaH - saidaH)
+        }else{
+            diferença += (saidaH - entradaH)
+        }
+        
+    }
+
+    preço = diferença * 15
+    
+    if(saidaM - entradaM > 0){
+        preço += 15
+    }
+
+    alert(`${preço}`)
 
     document.getElementById('saida').innerHTML = `O preço após ${diferença} horas é de: ${precoFinal}R$`
 
